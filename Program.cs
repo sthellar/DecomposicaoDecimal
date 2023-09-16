e950f8ca85259c0a59fcd3dbc510c4f682826c87
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-09-16 00:08:21 -0300

    Solução da atividade de decomposição decimal

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numeroDigitado, centenas, dezenas, unidades;
+
+Console.Write("Digite um número..: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"O número digitado possui:");
+
+centenas = numeroDigitado / 100;
+dezenas = (numeroDigitado % 100) / 10;
+unidades = (numeroDigitado % 100) % 10;
+
+Console.WriteLine($"{centenas} centena(s)");
+Console.WriteLine($"{dezenas} dezena(s)");
+Console.WriteLine($"{unidades} unidades(s)");
